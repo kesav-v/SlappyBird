b708479bdbdd2935697ad4c49d0f12a8d776a379
Ofek Gila <flamingo.ofek@gmail.com>
2015-05-28 20:19:09 -0700

    ghost default change

--- FlappyPanel.java
+++ FlappyPanel.java
@@ -29,9 +29,9 @@ import java.text.SimpleDateFormat;
 */
 
 public class FlappyPanel extends JPanel implements ActionListener, KeyListener {
-	
+							// initVel, null, null, numPipe, oscilPipe, changeOscil, ghostPipe, maxOscil, mOSpeed, numStartOscil
 	private final Object[] DEFAULT_VALUES = {(double)3, null, null, true, true, false, 400, (double)5, 1};
-	private final Object[] DEFAULT_GHOST = {(double)3, null, null, false, false, true, 300, (double)2, 2};
+	private final Object[] DEFAULT_GHOST = {(double)3, null, null, true, false, true, 300, (double)2, 2};
 	private FlappyPipe[] pipes;
 	private Timer movePipes;
 	private boolean first;
@@ -69,7 +69,7 @@ public class FlappyPanel extends JPanel implements ActionListener, KeyListener {
 		first = true;
 		firstPress = true;
 		headBangs = count = 0;
-		setValues(DEFAULT_VALUES);
+		setValues(DEFAULT_GHOST);
 		bird = new Bird(this);
 		addKeyListener(this);
 		justDied = true;
