9ca9639b9a2d1cfabdc2009cac9dd3a87a4b2b37
chess123 <kesav@viswanadha.net>
2015-06-01 20:59:21 -0700

    Changed FlappyBird.java

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,27 +1,12 @@
-import javax.swing.JFrame;
-import java.awt.Toolkit;
-import java.awt.Dimension;
-
 /**
  * This is the main class that runs the Flappy Bird game.
  * @author Kesav Viswanadha
- * @version 1.1
- * @lastedited May 23, 2015
+ * @version 2.1
+ * @lastedited June 1, 2015
 */
 
-public class FlappyBird extends JFrame {
-	
-	public FlappyBird() {
-		super("Flappy Bird");
-	}
-
+public class FlappyBird {
 	public static void main(String[] args) {
-		FlappyBird fw = new FlappyBird();
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		fw.setSize((int)d.getWidth(), (int)d.getHeight());
-		fw.setVisible(true);
-		//fw.getContentPane().add(new FlappyPanel());
-		fw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fw.setResizable(false);
+		TestMainMenu.main(new String[] {});
 	}
 }
